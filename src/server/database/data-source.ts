f0efec8ec4070d199e557d21bf3f5d6829692cdb
IvanBelshoff <ivanbelshoff@gmail.com>
2024-06-30 11:20:47 -0300

    fix: docker compose, data-source

--- src/server/database/data-source.ts
+++ src/server/database/data-source.ts
@@ -5,7 +5,9 @@ import { SeederOptions } from 'typeorm-extension';
 
 const port = process.env.DB_PORT as unknown as number;
 
-const options: DataSourceOptions & SeederOptions = {
+const host = process.env.DB_HOST;
+
+const options_db_nuvem: DataSourceOptions & SeederOptions = {
     type: 'postgres',
     host: process.env.DB_HOST,
     port: port,
@@ -23,4 +25,17 @@ const options: DataSourceOptions & SeederOptions = {
     migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
 };
 
-export const AppDataSource = new DataSource(options);
+
+const options_db_local: DataSourceOptions & SeederOptions = {
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: port,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    synchronize: true,
+    entities: [`${__dirname}/**/entities/*.{ts,js}`],
+    migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
+};
+
+export const AppDataSource = new DataSource(host == "database" ? options_db_local : options_db_nuvem);
