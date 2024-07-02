0b88caa9b24d6b5e484057666b1cc2d59a451171
ivanstriker13 <ivanbelshoff@gmail.com>
2024-07-02 15:39:22 -0300

    fix: URL

--- src/server/shared/middlewares/swaggerConfig.ts
+++ src/server/shared/middlewares/swaggerConfig.ts
@@ -11,7 +11,7 @@ const options = {
         },
         servers: [
             {
-                url: 'http://localhost:5032',
+                url: process.env.URL,
             },
         ],
         components: {
