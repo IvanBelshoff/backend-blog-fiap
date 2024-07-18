658317bd0c50fa47069b3ccacbed67af3a2c6bcd
IvanBelshoff <ivanbelshoff@gmail.com>
2024-07-18 17:14:46 -0300

    feat: faker library implementation in tests

--- tests/usuarios/SignIn.test.ts
+++ tests/usuarios/SignIn.test.ts
@@ -1,93 +1,130 @@
 import { StatusCodes } from 'http-status-codes';
+import { faker } from '@faker-js/faker/locale/pt_BR';
 import { testServer } from '../jest.setup';
+
+import { PasswordCrypto } from '../../src/server/shared/services';
+import { Usuario } from '../../src/server/database/entities';
+import { usuarioRepository } from '../../src/server/database/repositories';
 
 describe('Usuários - SignIn', () => {
 
+    const user = new Usuario();
+    const plainPassword = '123456'; // Definindo uma senha fixa para os testes
+
     beforeAll(async () => {
-        const user = await testServer.post('/usuarios').send({
-            nome: 'Jorge',
-            sobrenome: 'Santos',
-            senha: '123456',
-            email: 'jorge@gmail.com',
-            bloqueado: 'false'
+
+        const loginUserDefault = await testServer.post('/entrar').send({
+            senha: process.env.SENHA_USER_DEFAULT,
+            email: process.env.EMAIL_USER_DEFAULT,
         });
 
-        console.log(`Usuário teste criado: ${user}`);
+        const accessToken = loginUserDefault.body.accessToken;
+
+        user.nome = faker.person.firstName();
+        user.sobrenome = faker.person.lastName();
+        user.email = faker.internet.email({ firstName: user.nome, lastName: user.sobrenome });
+        user.bloqueado = false;
+        user.senha = await PasswordCrypto.hashPassword(plainPassword);
+
+        const createUser = await testServer.post('/usuarios')
+            .send({
+                nome: user.nome,
+                sobrenome: user.sobrenome,
+                senha: plainPassword, // Usando a senha em texto plano aqui
+                email: user.email,
+                bloqueado: user.bloqueado
+            })
+            .set({ Authorization: `Bearer ${accessToken}` });
+
+        console.log(`Usuário teste criado: ${JSON.stringify(createUser.body)}`);
+    });
+
+    afterAll(async () => {
+
+        const usuario = await usuarioRepository.findOne({ where: { email: user.email } });
+
+        if (usuario) {
+            const deleteUser = await usuarioRepository.delete({ id: usuario.id });
+            console.log(`Usuário teste deletado: ${deleteUser.affected}`);
+        } else {
+            console.log('Usuário teste não localizado');
+        }
+
     });
 
     it('Faz login', async () => {
         const res1 = await testServer
             .post('/entrar')
             .send({
-                senha: '123456',
-                email: 'jorge@gmail.com',
+                senha: plainPassword, // Usando a senha em texto plano aqui
+                email: user.email,
             });
 
         expect(res1.statusCode).toEqual(StatusCodes.OK);
         expect(res1.body).toHaveProperty('accessToken');
-
     });
+
     it('Senha errada', async () => {
         const res1 = await testServer
             .post('/entrar')
             .send({
                 senha: '1234567',
-                email: 'jorge@gmail.com',
+                email: user.email,
             });
         expect(res1.statusCode).toEqual(StatusCodes.UNAUTHORIZED);
         expect(res1.body).toHaveProperty('errors.default');
     });
+
     it('Email errado', async () => {
         const res1 = await testServer
             .post('/entrar')
             .send({
-                senha: '123456',
+                senha: plainPassword,
                 email: 'jorgeeeeeee@gmail.com',
             });
         expect(res1.statusCode).toEqual(StatusCodes.UNAUTHORIZED);
         expect(res1.body).toHaveProperty('errors.default');
     });
+
     it('Formato de email inválido', async () => {
         const res1 = await testServer
             .post('/entrar')
             .send({
-                senha: '123456',
+                senha: plainPassword,
                 email: 'jorge gmail.com',
             });
         expect(res1.statusCode).toEqual(StatusCodes.BAD_REQUEST);
         expect(res1.body).toHaveProperty('errors.body.email');
     });
+
     it('Senha muito pequena', async () => {
         const res1 = await testServer
             .post('/entrar')
             .send({
                 senha: '12',
-                email: 'jorge@gmail.com',
+                email: user.email,
             });
         expect(res1.statusCode).toEqual(StatusCodes.BAD_REQUEST);
         expect(res1.body).toHaveProperty('errors.body.senha');
     });
+
     it('Não informado a senha', async () => {
         const res1 = await testServer
             .post('/entrar')
             .send({
-                email: 'jorge@gmail.com',
+                email: user.email,
             });
         expect(res1.statusCode).toEqual(StatusCodes.BAD_REQUEST);
         expect(res1.body).toHaveProperty('errors.body.senha');
     });
+
     it('Não informado email', async () => {
         const res1 = await testServer
             .post('/entrar')
             .send({
-                senha: '123456',
+                senha: plainPassword,
             });
         expect(res1.statusCode).toEqual(StatusCodes.BAD_REQUEST);
         expect(res1.body).toHaveProperty('errors.body.email');
     });
-
-    afterAll(async () => {
-        console.log('Isso é tudo pessoal');
-    });
-
 });
