b1110c318e3edb98ad06ee170d67aaa8f471e38a
IvanBelshoff <ivanbelshoff@gmail.com>
2024-07-29 19:13:01 -0300

    fix: adjusted Create.test.ts

--- tests/usuarios/Create.test.ts
+++ tests/usuarios/Create.test.ts
@@ -3,12 +3,30 @@ import { Usuario } from '../../src/server/database/entities';
 import { faker } from '@faker-js/faker/locale/pt_BR';
 import { testServer } from '../jest.setup';
 import path from 'path';
+import fs from 'fs';
 
 describe('create', () => {
     const user = new Usuario();
     const plainPassword = '123456';
     let accessToken = '';
     let userId = '';
+
+    // Função auxiliar para encontrar o caminho correto do arquivo
+    const findFilePath = (filePaths: string[]): string | null => {
+        for (const filePath of filePaths) {
+            if (fs.existsSync(filePath)) {
+                return filePath;
+            }
+        }
+        return null;
+    };
+
+    const filePaths = [
+        path.resolve(__dirname, '..', '..', 'build/server/shared/data/default/profile.jpg'),
+        path.resolve(__dirname, '..', '..', 'src/server/shared/data/default/profile.jpg')
+    ];
+
+    const filePath = findFilePath(filePaths);
 
     beforeAll(async () => {
         const loginUserDefault = await testServer.post('/entrar').send({
@@ -20,13 +38,16 @@ describe('create', () => {
     });
 
     it('criar usuário e foto com sucesso', async () => {
+        if (!filePath) {
+            throw new Error('Profile picture file not found in any of the specified paths.');
+        }
 
         user.nome = faker.person.firstName();
         user.sobrenome = faker.person.lastName();
         user.email = faker.internet.email({ firstName: user.nome, lastName: user.sobrenome });
         user.bloqueado = false;
         user.senha = plainPassword;
-        
+
         const res1 = await testServer
             .post('/usuarios')
             .field('nome', user.nome)
@@ -34,7 +55,7 @@ describe('create', () => {
             .field('senha', user.senha || '')
             .field('email', user.email)
             .field('bloqueado', user.bloqueado)
-            .attach('foto', path.resolve(__dirname, '..', 'assets/profile.jpg'))
+            .attach('foto', filePath)
             .set({ Authorization: `Bearer ${accessToken}` });
 
         userId = res1.body;
