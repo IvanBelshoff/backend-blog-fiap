7ff2d16c71f1e9b773a56eddff110cc80813bf5e
Robson Nishikawa <takeshinishikawa@gmail.com>
2024-09-11 23:36:26 -0300

    fix: fixed post seeder to avoid titles over 50 chars and including images on post creation

--- src/server/database/factories/post.factory.ts
+++ src/server/database/factories/post.factory.ts
@@ -3,6 +3,13 @@ import { Foto, Postagem } from '../entities';
 import path from 'path';
 
 export default setSeederFactory(Postagem, async (faker) => {
+    const getRandomIntegerInclusive = (min: number, max: number) => {
+        min = Math.ceil(min)
+        max = Math.floor(max)
+        
+        return Math.floor(Math.random() * (max - min + 1)) + min
+    }
+    const randomInteger = getRandomIntegerInclusive(0, 200)
 
     const fotoLocal = process.env.SALVAR_FOTO_LOCAL as unknown as Boolean;
 
@@ -15,13 +22,14 @@ export default setSeederFactory(Postagem, async (faker) => {
     photo.nuvem = String(fotoLocal) == 'true' ? false : true,
     photo.width = 462,
     photo.height = 462,
-    photo.url = String(fotoLocal) == 'true' ? `http://${process.env.HOST}:${process.env.PORT}/profile/profile.jpg` : 'https://firebasestorage.googleapis.com/v0/b/blog-fiap.appspot.com/o/profile.jpg?alt=media',
+    photo.url = String(fotoLocal) == 'true' ? `https://loremflickr.com/400/200?random=${randomInteger}` : 'https://firebasestorage.googleapis.com/v0/b/blog-fiap.appspot.com/o/profile.jpg?alt=media',
+    // photo.url = String(fotoLocal) == 'true' ? `http://${process.env.HOST}:${process.env.PORT}/profile/profile.jpg` : 'https://firebasestorage.googleapis.com/v0/b/blog-fiap.appspot.com/o/profile.jpg?alt=media',
     photo.tipo = 'jpg';
 
     const post = new Postagem();
 
-    post.titulo = faker.lorem.sentence({min: 30, max: 50});
-    post.conteudo = faker.lorem.paragraphs({min: 1, max: 5});
+    post.titulo = faker.lorem.sentence().substring(0, 50);
+    post.conteudo = faker.lorem.paragraphs({min: 3, max: 5});
     post.visivel = faker.datatype.boolean();
     post.usuario_atualizador = 'seed';
     post.usuario_cadastrador = 'seed';
